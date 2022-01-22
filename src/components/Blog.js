23fde50ca0379cb7a13f117e3521e5424f6c61b6
Iamsej <iamsamueljones@gmail.com>
2022-01-21 19:26:36 -0500

    5.7 functional

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return(
+  <>
+  <div className='blog' style={hideWhenVisible}>
+    {blog.title} {blog.author} {}
+    <button onClick={toggleVisibility}>view</button>
+  </div>
+  <div className='blog' style={showWhenVisible}>
+    {blog.title} {blog.author} {}
+    <button onClick={toggleVisibility}>hide</button>
+    <br/>
+    {blog.url}
+    <br/>
+    {blog.likes} {}
+    <button>like</button>
+    <br/>
+    {blog.user.username}
+  </div>
+  </>
+  )  
+}
 
 export default Blog
